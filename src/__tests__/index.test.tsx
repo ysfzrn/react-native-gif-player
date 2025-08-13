3ea5d1f79408cef6497be1768c73c32a7a7d6c9b
Yusuf Zeren <ysfzrn@gmail.com>
2025-08-13 03:56:28 +0300

    test: add jumpToFrame command test

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,1 +1,22 @@
-it.todo('write a test');
+import { GifPlayerView } from '../index';
+import { Commands } from '../GifPlayerViewNativeComponent';
+
+describe('GifPlayerView', () => {
+  it('calls native jumpToFrame command with provided frame', () => {
+    const jumpToFrameMock = jest
+      .spyOn(Commands, 'jumpToFrame')
+      .mockImplementation(jest.fn());
+
+    const component = new GifPlayerView({
+      paused: true,
+      source: { uri: '' },
+    } as any);
+
+    (component as any).innerRef = { current: {} };
+    component.render();
+
+    component.jumpToFrame(5);
+
+    expect(jumpToFrameMock).toHaveBeenCalledWith(expect.anything(), 5);
+  });
+});
